466caca2d2845b2505785a6f65fac8b2c1c5e566
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-09 20:26:24 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,41 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+import React from "react";
 import './Weather.css';
 
 export default function Weather() {
+    // let [city, newCity] = useState("Kharkiv");
+    // let [temperature, newTemperature] = useState(" ");
+    // let [humidity, newHumidity] = useState("");
+    // let [wind, newWind] = useState(" ");
+    // let [description, newDescription] = useState(" ");
+    // let [icon, newIcon] = useState("");
+
+
+    // function Submit(event) {
+    //     event.preventDefault();
+    //     let key = "f81614abe2395d5dfecd45b9298041de";
+    //     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+    //     axios.get(url).then(WeatherForecast); 
+    // }
+    // function Search(event) {
+    //     newCity(event.target.value); 
+    // }
+    // function WeatherForecast(response) {
+    //     newTemperature = Math.round(response.data.main.temp);
+    //     newHumidity = response.data.weather[0].description;
+    //     newWind = Math.round(response.data.wind.speed);
+    //     newDescription = response.data.weather[0].description;
+    //     newIcon = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+    // }
+
     return (
          <div className="weatherdiv">
             {/* <!-- HEADER  --> */}
             <div>
-                <form className="search-form">
+                <form className="search-form"
+                    // onSubmit={Submit}
+                >
                     <div className="row">
                         <div className="col-md-12 col-lg-8">
                             <input
@@ -14,7 +44,9 @@ export default function Weather() {
                                 autoFocus="on"
                                 autoComplete="off"
                                 id="city"
-                                className="form-control shadow-sm city" />
+                                className="form-control shadow-sm city"
+                                // onChange={Search}
+                            />
                         </div>
                         <div className="col-sm-12 col-lg-4">
                             <div className="row row-cols-auto">
@@ -42,29 +74,27 @@ export default function Weather() {
 
                 <div className="col-sm-12 col-md-6 col-lg-4 temperature">
                     <p>
-                        <span id="tempNow">28</span>
+                        <span id="tempNow">20</span>
                         <span className="temp-cf">
                             <button id="celsius" className="active-link">℃</button> |
                             <button id="fahrenheit">℉</button>
-                            {/* <a href="#" id="celsius" className="active-link">℃</a> |
-                            <a href="#" id="fahrenheit">℉</a> */}
                         </span>
                     </p>
                 </div>
 
                 <div className="col-sm-12 col-md-6 col-lg-4 weather-description">
                     <div>
-                        <img src="" alt="clear" id="main-icon" />
+                        <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="clear" id="main-icon" />
                         <p id="sky-now" className="d-inline">scattered clouds</p>
                     </div>
                     <p className="ms-4">
                         <i className="fa-solid fa-wind windNow"></i>Wind:
-                        <span className="wind-main">3</span>
+                        <span className="wind-main"> 3</span>
                         <span className="units">km/h</span>
                     </p>
                     <p className="ms-4">
-                        <i className="fa-solid fa-droplet"></i> Humidity:
-                        <span className="humidity-main">15</span>
+                        <i className="fa-solid fa-droplet"></i>Humidity:
+                        <span className="humidity-main"> 15</span>
                         <span className="units">%</span>
                     </p>
                 </div>
