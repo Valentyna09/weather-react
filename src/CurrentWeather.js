698be545a2fe005880271c50d91dc33216c36df1
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-10 18:24:12 +0300

    created functional form of search city

---
+++ src/CurrentWeather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import LastUpdate from "./LastUpdate";
+
+export default function CurrentWeather(props) {
+    return (
+            <div className="row current-weather">
+                <div className="col-sm-12 col-lg-4 mt-3">
+                       <h1 id="city-name" className="text-center">{props.data.city}</h1>
+                    <p className="date">
+                           Last update: <br />
+                           <span id="currentDayTime">
+                               <LastUpdate date={props.data.date} />
+                           </span>
+                    </p>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-4 temperature">
+                    <p>
+                        <span id="tempNow">{Math.round(props.data.temperature)}</span>
+                        <span className="temp-cf">
+                            <button id="celsius" className="active-link">℃</button> |
+                            <button id="fahrenheit">℉</button>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-4 weather-description">
+                    <div>
+                        <img src={props.data.icon} alt="clear" id="main-icon" />
+                           <p id="sky-now" className="d-inline">{props.data.description}</p>
+                    </div>
+                    <p className="ms-4">
+                        <i className="fa-solid fa-wind windNow"></i>Wind:
+                           <span className="wind-main"> {Math.round(props.data.wind)} </span>
+                        <span className="units">km/h</span>
+                    </p>
+                    <p className="ms-4">
+                        <i className="fa-solid fa-droplet"></i>Humidity:
+                           <span className="humidity-main"> {Math.round(props.data.humidity)} </span>
+                        <span className="units">%</span>
+                    </p>
+                </div>
+            </div>
+    );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import './Weather.css';
-import LastUpdate from "./LastUpdate";
-
-    // function Submit(event) {
-    //     event.preventDefault();
-    //     let key = "f81614abe2395d5dfecd45b9298041de";
-    //     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
-    //     axios.get(url).then(WeatherForecast);
-    // }
-    // function Search(event) {
-    //     newCity(event.target.value);
-    // }
+import CurrentWeather from "./CurrentWeather";
 
 export default function Weather(props) {
     let [weatherData, newWeatherData] = useState({ ready: false });
+    const [city, newCity] = useState(props.defaultCity);
     
     function weatherForecast(response) {
         console.log(response.data);
@@ -29,15 +20,27 @@ export default function Weather(props) {
             date: new Date(response.data.dt*1000)
         });
     }
+    function Search() {
+        let key = "f81614abe2395d5dfecd45b9298041de";
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+        axios.get(url).then(weatherForecast); 
+    }
 
+    function Submit(event) {
+        event.preventDefault();
+        Search();
+    }
+
+    function chooseCity(event) {
+        newCity(event.target.value);
+    }
+    
     if (weatherData.ready) {
        return (
          <div className="weatherdiv">
             {/* <!-- HEADER  --> */}
             <div>
-                <form className="search-form"
-                    // onSubmit={Submit}
-                >
+                <form className="search-form" onSubmit={Submit}>
                     <div className="row">
                         <div className="col-md-12 col-lg-8">
                             <input
@@ -46,9 +49,7 @@ export default function Weather(props) {
                                 autoFocus="on"
                                 autoComplete="off"
                                 id="city"
-                                className="form-control shadow-sm city"
-                                // onChange={Search}
-                            />
+                                className="form-control shadow-sm city" onChange={chooseCity}/>
                         </div>
                         <div className="col-sm-12 col-lg-4">
                             <div className="row row-cols-auto">
@@ -64,46 +65,9 @@ export default function Weather(props) {
                         </div>
                     </div>
                 </form>
-            </div>
-            {/* <!-- Current weather  --> */}
-            <div className="row current-weather">
-                <div className="col-sm-12 col-lg-4 mt-3">
-                       <h1 id="city-name" className="text-center">{weatherData.city}</h1>
-                    <p className="date">
-                           Last update: <br />
-                           <span id="currentDayTime">
-                               <LastUpdate date={ weatherData.date} />
-                           </span>
-                    </p>
-                </div>
+               </div>
+               <CurrentWeather data={weatherData} />
 
-                <div className="col-sm-12 col-md-6 col-lg-4 temperature">
-                    <p>
-                        <span id="tempNow">{Math.round(weatherData.temperature)}</span>
-                        <span className="temp-cf">
-                            <button id="celsius" className="active-link">℃</button> |
-                            <button id="fahrenheit">℉</button>
-                        </span>
-                    </p>
-                </div>
-
-                <div className="col-sm-12 col-md-6 col-lg-4 weather-description">
-                    <div>
-                        <img src={weatherData.icon} alt="clear" id="main-icon" />
-                           <p id="sky-now" className="d-inline">{weatherData.description}</p>
-                    </div>
-                    <p className="ms-4">
-                        <i className="fa-solid fa-wind windNow"></i>Wind:
-                           <span className="wind-main"> {Math.round(weatherData.wind)} </span>
-                        <span className="units">km/h</span>
-                    </p>
-                    <p className="ms-4">
-                        <i className="fa-solid fa-droplet"></i>Humidity:
-                           <span className="humidity-main"> {Math.round(weatherData.humidity)} </span>
-                        <span className="units">%</span>
-                    </p>
-                </div>
-            </div>
 
             {/* <!-- Weather cards  --> */}
             <div id="weather-forecast-cards">
@@ -112,10 +76,7 @@ export default function Weather(props) {
         </div>
     ); 
     } else {
-        let key = "f81614abe2395d5dfecd45b9298041de";
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${key}`;
-        axios.get(url).then(weatherForecast); 
-        
+        Search();
         return "Loading..."
     }
 }
