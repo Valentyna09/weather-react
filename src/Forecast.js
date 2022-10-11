98aa9f5e2d793e8c574f59d94f4432049791cad1
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-11 23:09:05 +0300

    loops forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -12,43 +12,13 @@ export default function Forecast(props) {
     }
 
     if (loaded) {
-                console.log(forecast);
         return (
             <div className="row">
-                <ForecastDays infoWeather={forecast[0]} />
-                <ForecastDays infoWeather={forecast[1]} />
-                <ForecastDays infoWeather={forecast[2]} />
-                <ForecastDays infoWeather={forecast[3]} />
-                <ForecastDays infoWeather={forecast[4]} />
-                {/* <div className="col-lg-2 col-md-5 div-cards ">
-                    <h2 className="days">{forecast[0].dt}</h2>
-                    <div className ="card">
-                        <div className ="card-body">
-                            <Icon icon={forecast[0].weather[0].icon} size={55} />
-                            <h2 className="days">{forecast[0].weather[0].description}</h2>
-                            <p className ="card-text">
-                                <i className ="fa-solid fa-temperature-low" title="temperature"></i>
-                                <span className="day-temp" title="day temperature">{Math.round(forecast[0].temp.max)}</span>
-                                <span className ="celsius">℃ </span>
-                                <span className ="separate">| </span>
-                                <span className ="night-temp" title="night temperature">{Math.round(forecast[0].temp.min)}</span>
-                                <span className ="celsius">℃</span>
-                            </p>
-                            <p className ="card-text">
-                                <i className ="fa-solid fa-wind" title="wind"></i>
-                                <span className ="wind">Wind: </span>
-                                <span className ="wind">{Math.round(forecast[0].wind_speed)} </span>
-                                <span className ="units">km/h</span>
-                            </p>
-                            <p className ="card-text">
-                                <i className ="fa-solid fa-droplet" title="humidity"></i>
-                                <span className ="humidity">Humidity: </span>
-                                <span className ="humidity">{Math.round(forecast[0].humidity)} </span>
-                                <span className ="units">%</span>
-                            </p>
-                        </div>
-                    </div>
-                </div> */}
+                {forecast.map(function (everydayForecast, index) {
+                    if (index < 5) {
+                        return <ForecastDays key={index} infoWeather={everydayForecast} />;
+                    }
+                })}
             </div>
         );
     } else {
