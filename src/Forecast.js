ab6a5527c251805dfaeab6a786684c4408bc0cd0
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-11 23:41:52 +0300

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import axios from "axios";
 import ForecastDays from "./ForecastDays";
+import { cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, newForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
 
     function weatherForecast(response) {
         newForecast(response.data.daily);
