e9eefc93ef2dcbb48a6e60dd4c65354b714cfa54
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-11 23:53:19 +0300

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ForecastDays from "./ForecastDays";
-import { cleanup } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
@@ -17,24 +15,29 @@ export default function Forecast(props) {
         setLoaded(true);
     }
 
-    if (loaded) {
-        return (
-            <div className="row">
-                {forecast.map(function (everydayForecast, index) {
-                    if (index < 5) {
-                        return <ForecastDays key={index} infoWeather={everydayForecast} />;
-                    }
-                })}
-            </div>
-        );
-    } else {
+    function searchCity() {
         let key = "f5029b784306910c19746e40c14d6cd3";
         let lon = props.coordinates.lon;
         let lat = props.coordinates.lat;
         let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
 
         axios.get(url).then(weatherForecast);
+    }
 
+    if (loaded) {
+        return (
+            <div className="row">
+                {forecast.map(function (everydayForecast, index) {
+                    if (index < 5) {
+                        return <ForecastDays key={index} infoWeather={everydayForecast} />;
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+        );
+    } else {
+        searchCity()
         return null; 
     }
 }
